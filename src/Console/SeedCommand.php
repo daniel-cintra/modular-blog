692acb42c7cb61836028490cc619bcdeffce0158
Daniel Cintra <danic10@gmail.com>
2024-02-19 22:20:09 -0300

    Add the Blog Acl Seeder to the Seed Command.

--- src/Console/SeedCommand.php
+++ src/Console/SeedCommand.php
@@ -4,7 +4,7 @@ namespace Modular\Blog\Console;
 
 use Illuminate\Console\Command;
 use Modules\Blog\Database\Seeders\BlogSeeder;
-
+use Modules\Blog\Database\Seeders\BlogAclSeeder;
 class SeedCommand extends Command
 {
     protected $signature = 'modular:blog-seed';
@@ -13,9 +13,10 @@ class SeedCommand extends Command
 
     public function handle(): void
     {
-        $this->info('Running the following command to seed the Blog module:');
+        $this->info('Running the following commands to seed the Blog Module:');
 
         $this->call(BlogSeeder::class);
+        $this->call(BlogAclSeeder::class);
 
         $this->info('Modular Blog seeded successfully.');
     }
