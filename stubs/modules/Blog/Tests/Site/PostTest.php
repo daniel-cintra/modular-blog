c12621b80b6de25e3f32a817d23ca7b3ed75281c
Daniel Cintra <danic10@gmail.com>
2024-10-30 16:55:48 -0300

    Updates post test.

--- stubs/modules/Blog/Tests/Site/PostTest.php
+++ stubs/modules/Blog/Tests/Site/PostTest.php
@@ -8,17 +8,33 @@ use Tests\TestCase;
 uses(TestCase::class, RefreshDatabase::class);
 
 beforeEach(function () {
-    $this->post = Post::factory()->create();
+    $this->post = Post::factory()->create([
+        'title' => 'Test Post',
+        'content' => 'Test Content',
+        'published_at' => now()->subDay(),
+    ]);
 });
 
 afterEach(function () {
     if ($this->post->image) {
-        Storage::disk('public')->delete('blog/'.$this->post->image);
+        Storage::disk('public')->delete('blog/' . $this->post->image);
     }
 });
 
 test('blog index page can be rendered', function () {
+    $this->withoutVite();
     $response = $this->get('/blog');
 
     $response->assertStatus(200);
+    $response->assertSee('Blog');
+    $response->assertSee($this->post->title);
+});
+
+test('blog post page can be rendered', function () {
+    $this->withoutVite();
+    $response = $this->get('/blog/' . $this->post->slug);
+
+    $response->assertStatus(200);
+    $response->assertSee($this->post->title);
+    $response->assertSee($this->post->content);
 });
